55d5d9614bfaa94af816879fe597ac8da2a6a385
Alexander Leutgeb <aleutgeb@users.noreply.github.com>
2022-03-03 17:56:38 +0100

    Implemented offset curve computation.
    Implemented offset curve computation.

--- README.md
+++ README.md
@@ -1,12 +1,16 @@
 # STEPToXSection
-The program STEPToXSection extracts the contour of a planar cross section of solids contained in STEP files.
+The program STEPToXSection extracts the contour of a planar cross section of solids contained in STEP files. Additionally it supports offsetting of the cross section contour.
 
 # Description
-The program STEPToXSection is a command line utility to export the contour of a planar cross section of solids contained in STEP files. The contour is a list of line segments. The supported output file formats are ply and xyz. In the case of xyz, two consecutive vertices belong to the same edge.  STEPToXSection is based on OpenCASCADE (https://www.opencascade.com). The program uses cxxops (https://github.com/jarro2783/cxxopts) for parsing the command line.
+The program STEPToXSection is a command line utility to export the contour of a planar cross section of solids contained in STEP files. Additionally it supports offsetting of the cross section contour. The contour is a list of line segments. The supported output file formats are ply and xyz. In the case of xyz, two consecutive vertices belong to the same edge.  STEPToXSection is based on OpenCASCADE (https://www.opencascade.com). The program uses cxxops (https://github.com/jarro2783/cxxopts) for parsing the command line.
 
 | Solids | Planar Cross-Section |
-| :---:  | :---:                |
-| ![Image Solids](examples/basic_shapes/basic_shapes_solids.png) | ![Image Cross-Section](examples/basic_shapes/basic_shapes_cross-section.png) |
+| :---:  | :---: |
+| ![Image Solids](examples/spheres/solids.png) | ![Image Cross-Section](examples/spheres/cross_section.png) |
+| :---:  | :---: |
+| Positive Offset Curves | Negative Offset Curves |
+| :---:  | :---: |
+| ![Image Positive-Offset-Curves](examples/spheres/positive_offset_curves.png) | ![Image Negative-Offset-Curves](examples/spheres/negative_offset_curves.png) |
 
 # Requirements
  * CMake installation (https://cmake.org)
@@ -42,6 +46,7 @@ Usage:
   -c, --content         List content (solids)
   -s, --select arg      Select solids by name or index (comma seperated list, index starts with 1)
   -d, --deflection arg  deflection
+  -t, --offset arg      offset (default: 0.0)
   -p, --plane arg       Plane (a,b,c,d), in which a*x + b*y + c*z + d = 0
   -h, --help            Print usage
 ```
@@ -50,4 +55,4 @@ Usage:
  * See `examples` directory
  
 # Remarks
-This code has been tested with an OpenCASCADE 7.4.0 prebuilt binary (`opencascade-7.4.0-vc14-64.exe`). With changes in the configuration section in the `CMakeLists.txt` file the build should also work with other OpenCASCADE versions. For building on Linux the `CMakeLists.txt` file needs further adaptions.
+This code has been tested with an OpenCASCADE 7.5.0 prebuilt binary (`opencascade-7.5.0-vc14-64.exe`) on Windows, as well as OpenCASCADE system packages on openSUSE Linux. With changes in the configuration section in the `CMakeLists.txt` file the build should also work with other OpenCASCADE versions.
--- src/STEPToXSection.cpp
+++ src/STEPToXSection.cpp
@@ -3,7 +3,7 @@
 //
 // Description:
 //
-// The program STEPToXSection extracts the contour of a planar cross section of solids contained in STEP files.
+// The program STEPToXSection extracts the contour of a planar cross section of solids contained in STEP files. Additionally it supports offsetting of the cross section contour.
 //
 // Copyright(C) 2021 Alexander Leutgeb
 //
@@ -36,11 +36,27 @@
 #include <TDF_ChildIterator.hxx>
 #include <TDataStd_Name.hxx>
 #include <BRepBuilderAPI_Transform.hxx>
+#include <BRepBuilderAPI_MakeWire.hxx>
+#include <BRepBuilderAPI_MakeFace.hxx>
+#include <BRepBuilderAPI_MakePolygon.hxx>
+#include <ShapeAnalysis_FreeBounds.hxx>
 #include <BRepAlgoAPI_Section.hxx>
+#include <BRepAlgoAPI_Fuse.hxx>
+#include <BRepAlgoAPI_Cut.hxx>
 #include <BRepAdaptor_Curve.hxx>
+#include <BRepOffsetAPI_MakeOffset.hxx>
+#include <BRepCheck_Analyzer.hxx>
+#include <BRepTools.hxx>
+#include <BRepTools_WireExplorer.hxx>
+#include <BRep_Tool.hxx>
+#include <BRepFeat.hxx>
+#include <GeomLProp_SLProps.hxx>
+#include <ShapeUpgrade_UnifySameDomain.hxx>
 #include <gp_Pln.hxx>
 #include <GCPnts_UniformDeflection.hxx>
 #include <vector>
+#include <unordered_map>
+#include <deque>
 #include <array>
 #include <cmath>
 #include <iostream>
@@ -97,26 +113,223 @@ void read(const std::string& inFile, std::vector<NamedSolid>& namedSolids) {
 	}
 }
 
-auto computeXSection(const TopoDS_Compound& compound, const std::array<double, 4>& plane, const double deflection) -> std::vector<std::array<double, 3>> {
-	std::vector<std::array<double, 3>> points;
-	BRepAlgoAPI_Section section(compound, gp_Pln(plane[0], plane[1], plane[2], plane[3]), Standard_False);
+auto convertToPolygon(const TopoDS_Wire& wire, const gp_Pln& xsectionPlane, const double deflection) -> TopoDS_Wire {
+	BRepBuilderAPI_MakePolygon makePolygon;
+	BRepTools_WireExplorer wireExplorer;
+	for (wireExplorer.Init(wire); wireExplorer.More(); wireExplorer.Next()) {
+		TopoDS_Edge edge{wireExplorer.Current()};
+		BRepAdaptor_Curve curve{edge};
+		GCPnts_UniformDeflection discretizer{curve, deflection, Standard_True};
+		if (discretizer.IsDone() && discretizer.NbPoints() > 0) {
+			int nbPoints = discretizer.NbPoints();
+			std::vector<gp_Pnt> points;
+			for (int i{1}; i <= nbPoints; i++) {
+				gp_Pnt p{discretizer.Value(i)};
+				// p lies on xsection plane, but BRepOffsetAPI_MakeOffset needs a more exact positioning. So we move p closer to the plane.
+				gp_XYZ coord{p.Coord()};
+				coord += xsectionPlane.Axis().Direction().XYZ().Multiplied(xsectionPlane.Distance(p));
+				p.SetCoord(coord.X(), coord.Y(), coord.Z());
+				points.emplace_back(p.X(), p.Y(), p.Z());
+			}
+			const gp_Pnt startPoint{BRep_Tool::Pnt(wireExplorer.CurrentVertex())};
+			if (points.size() > 0) {
+				if (points.back().Distance(startPoint) < points.front().Distance(startPoint)) std::reverse(std::begin(points), std::end(points));
+				for (auto i{1u}; i < points.size(); ++i) makePolygon.Add(points[i]);
+			}
+		}
+	}
+	makePolygon.Close();
+	return makePolygon.Wire();
+}
+
+auto convertToWires(const TopoDS_Shape& shape) -> std::vector<TopoDS_Wire> {
+	std::vector<TopoDS_Wire> result;
+	Handle(TopTools_HSequenceOfShape) edges{new TopTools_HSequenceOfShape{}};
+	Handle(TopTools_HSequenceOfShape) wires{new TopTools_HSequenceOfShape{}};
+	TopExp_Explorer explorer;
+	for (explorer.Init(shape, TopAbs_EDGE); explorer.More(); explorer.Next())
+	{
+		edges->Append(TopoDS::Edge(explorer.Current()));
+	}
+	ShapeAnalysis_FreeBounds::ConnectEdgesToWires(edges, 0.0, Standard_True, wires);
+	for (int iWire{1}; iWire <= wires->Size(); ++iWire) result.emplace_back(TopoDS::Wire(wires->Value(iWire)));
+	return result;
+}
+
+auto computeXSectionWires(const TopoDS_Compound& compound, const gp_Pln& xsectionPlane) -> std::vector<TopoDS_Wire> {
+	BRepAlgoAPI_Section section(compound, xsectionPlane, Standard_False);
 	section.ComputePCurveOn1(Standard_True);
 	section.Approximation(Standard_True);
 	section.Build();
-	std::list<TopoDS_Edge> edges;
+	return convertToWires(section.Shape());
+}
+
+void discretizeWire(const TopoDS_Wire& wire, std::vector<std::array<double, 3>>& points, const double deflection) {
 	TopExp_Explorer explorer;
-	for (explorer.Init(section.Shape(), TopAbs_EDGE); explorer.More(); explorer.Next()) {
+	for (explorer.Init(wire, TopAbs_EDGE); explorer.More(); explorer.Next()) {
 		TopoDS_Edge edge{TopoDS::Edge(explorer.Current())};
 		BRepAdaptor_Curve curve{edge};
 		GCPnts_UniformDeflection discretizer(curve, deflection, Standard_True);
 		if (discretizer.IsDone() && discretizer.NbPoints() > 0) {
 			int nbPoints = discretizer.NbPoints();
 			for (int i = 2; i <= nbPoints; i++) {
-				const gp_Pnt p0 = discretizer.Value(i - 1);
-				const gp_Pnt p1 = discretizer.Value(i);
+				const gp_Pnt p0{discretizer.Value(i - 1)};
+				const gp_Pnt p1{discretizer.Value(i)};
 				points.emplace_back(std::array<double, 3>{p0.X(), p0.Y(), p0.Z()});
 				points.emplace_back(std::array<double, 3>{p1.X(), p1.Y(), p1.Z()});
 			}
+		}
+	}
+}
+
+auto surfaceNormal(const TopoDS_Face& face) -> gp_Dir {
+	Standard_Real umin, umax, vmin, vmax;
+	BRepTools::UVBounds(face, umin, umax, vmin, vmax);
+	Handle(Geom_Surface) surface {BRep_Tool::Surface(face)};
+	GeomLProp_SLProps props(surface, umin, vmin, 1, 0.01);
+	return props.Normal();
+}
+
+auto cutTwoFaces(const TopoDS_Face& origFace0, const TopoDS_Face& origFace1) -> std::vector<TopoDS_Face> {
+	std::vector<TopoDS_Face> result;
+	TopTools_ListOfShape arguments;
+	TopTools_ListOfShape tools;
+	TopoDS_Face face0{origFace0};
+	TopoDS_Face face1{origFace1};
+	if (surfaceNormal(face0).Dot(surfaceNormal(face1)) < 0.0) face1.Reverse();
+	arguments.Append(face0);
+	tools.Append(face1);
+	BRepAlgoAPI_Cut cut;
+	cut.SetArguments(arguments);
+	cut.SetTools(tools);
+	cut.Build();
+	TopoDS_Shape shape = cut.Shape();
+	ShapeUpgrade_UnifySameDomain unify(shape, Standard_True, Standard_True, Standard_False);
+	unify.Build();
+	shape = unify.Shape();
+	for (TopoDS_Iterator iter(shape); iter.More(); iter.Next()) {
+		if (iter.Value().ShapeType() == TopAbs_FACE) {
+			result.emplace_back(TopoDS::Face(iter.Value()));
+		}
+	}
+	return result;
+}
+
+auto uniteTwoFaces(const TopoDS_Face& origFace0, const TopoDS_Face& origFace1) -> std::vector<TopoDS_Face> {
+	std::vector<TopoDS_Face> result;
+	TopTools_ListOfShape arguments;
+	TopTools_ListOfShape tools;
+	TopoDS_Face face0{origFace0};
+	TopoDS_Face face1{origFace1};
+	if (surfaceNormal(face0).Dot(surfaceNormal(face1)) < 0.0) face1.Reverse();
+	arguments.Append(face0);
+	tools.Append(face1);
+	BRepAlgoAPI_Fuse fuse;
+	fuse.SetArguments(arguments);
+	fuse.SetTools(tools);
+	fuse.Build();
+	TopoDS_Shape shape = fuse.Shape();
+	ShapeUpgrade_UnifySameDomain unify(shape, Standard_True, Standard_True, Standard_False);
+	unify.Build();
+	shape = unify.Shape();
+	for (TopoDS_Iterator iter(shape); iter.More(); iter.Next()) {
+		if (iter.Value().ShapeType() == TopAbs_FACE) {
+			result.emplace_back(TopoDS::Face(iter.Value()));
+		}
+	}
+	return result;
+}
+
+void uniteFaces(const std::vector<TopoDS_Face>& faces, std::vector<std::array<double, 3>>& points, const double deflection) {
+	std::deque<TopoDS_Face> work;
+	std::copy(std::begin(faces), std::end(faces), std::back_inserter(work));
+	while (!work.empty()) {
+		TopoDS_Face face{work.front()};
+		work.pop_front();
+		bool united{false};
+		for (auto iter{std::begin(work)}; iter != std::end(work); ++iter) {
+			std::vector<TopoDS_Face> result{uniteTwoFaces(face, *iter)};
+			if (result.size() == 1) {
+				united = true;
+				work.erase(iter);
+				work.push_back(result[0]);
+				break;
+			}
+		}
+		if (!united) {
+			for (const auto& wire : convertToWires(face)) discretizeWire(wire, points, deflection);
+		}
+	}
+}
+
+auto computeContainment(const std::vector<TopoDS_Wire>& wires) -> std::unordered_map<int, std::vector<int>> {
+	std::unordered_map<int, std::vector<int>> result;
+	std::vector<bool> contained(wires.size(), false);
+	std::vector<std::vector<int>> contains(wires.size());
+	for (auto iOuter{0u}; iOuter < wires.size(); ++iOuter) {
+		TopoDS_Face outerFace{BRepBuilderAPI_MakeFace{wires[iOuter]}.Face()};
+		for (auto iInner{0u}; iInner < wires.size(); ++iInner) {
+			if (iOuter != iInner) {
+				TopoDS_Face innerFace{BRepBuilderAPI_MakeFace{wires[iInner]}.Face()};
+				if (BRepFeat::IsInside(innerFace, outerFace)) {
+					contained[iInner] = true;
+					result[iOuter].emplace_back(iInner);
+				}
+			}
+		}
+	}
+	for (auto iOuter{0u}; iOuter < wires.size(); ++iOuter) {
+		if (!contained[iOuter] && result.find(iOuter) == std::end(result)) result[iOuter];
+	}
+	return result;
+}
+
+auto computeOffsetWire(const TopoDS_Wire& wire, const double offset) -> std::optional<TopoDS_Wire> {
+	std::optional<TopoDS_Wire> result;
+	BRepOffsetAPI_MakeOffset makeOffset{wire, GeomAbs_Arc};
+	makeOffset.Perform(offset);
+	if (makeOffset.IsDone()) {
+		const TopoDS_Shape& shape{makeOffset.Shape()};
+		if (!shape.IsNull() && shape.ShapeType() == TopAbs_WIRE) {
+			result = TopoDS::Wire(shape);
+		}
+	}
+	else throw std::runtime_error{ "Invalid offset operation" };
+	return result;
+}
+
+auto computeXSection(const TopoDS_Compound& compound, const std::array<double, 4>& plane, const double deflection, const double offset) -> std::vector<std::array<double, 3>> {
+	std::vector<std::array<double, 3>> points;
+	const gp_Pln xsectionPlane{plane[0], plane[1], plane[2], plane[3]};
+	std::vector<TopoDS_Wire> wires{computeXSectionWires(compound, xsectionPlane)};
+	if (std::abs(offset) < 0.1 * deflection) {
+		for (const auto& wire : wires) discretizeWire(wire, points, deflection);
+	}
+	else {
+		std::vector<TopoDS_Wire> polygonWires;
+		for (const auto& wire : wires) polygonWires.emplace_back(convertToPolygon(wire, xsectionPlane, deflection));
+		std::unordered_map<int, std::vector<int>> containment{computeContainment(polygonWires)};
+		std::vector<TopoDS_Face> offsetFaces;
+		for (const auto entry : containment) {
+			std::optional<TopoDS_Wire> outerWire{computeOffsetWire(polygonWires[entry.first], offset)};
+			if (outerWire) {
+				BRepBuilderAPI_MakeFace makeFace{*outerWire};
+				TopoDS_Face face{makeFace.Face()};
+				for (const auto inner : entry.second) {
+					std::optional<TopoDS_Wire> innerWire{computeOffsetWire(polygonWires[inner], -offset) };
+					if (innerWire) {
+						std::vector<TopoDS_Face> cut{cutTwoFaces(face, BRepBuilderAPI_MakeFace{*innerWire}.Face())};
+						if (cut.size() == 1) face = cut[0];
+					}
+				}
+				offsetFaces.emplace_back(face);
+			}
+		}
+		if (offsetFaces.size() == 1) {
+			for (const auto& wire : convertToWires(offsetFaces[0])) discretizeWire(wire, points, deflection);
+		}
+		else if (offsetFaces.size() > 1) {
+			uniteFaces(offsetFaces, points, deflection);
 		}
 	}
 	return points;
@@ -153,7 +366,7 @@ void writePLY(const std::string& outFile, const std::array<double, 4>& plane, co
 }
 
 void write(const std::string& outFile, const std::vector<NamedSolid>& namedSolids, const std::vector<std::string>& select,
-		const double deflection, const std::array<double, 4>& plane, const std::string& format) {
+		const double deflection, const double offset, const std::array<double, 4>& plane, const std::string& format) {
 	TopoDS_Compound compound;
 	TopoDS_Builder builder;
 	builder.MakeCompound(compound);
@@ -181,13 +394,13 @@ void write(const std::string& outFile, const std::vector<NamedSolid>& namedSolid
 			}
 		}
 	}
-	std::vector<std::array<double, 3>> points{computeXSection(compound, plane, deflection)};
+	std::vector<std::array<double, 3>> points{computeXSection(compound, plane, deflection, offset)};
 	if (format == "xyz") writeXYZ(outFile, plane, points);
 	else if (format == "ply") writePLY(outFile, plane, points);
 }
 
 int main(int argc, char* argv[]) {
-	cxxopts::Options options{"STEPToXSection", "Extracts the contour of a planar cross section of solids contained in the STEP file"};
+	cxxopts::Options options{"STEPToXSection", "Extracts the contour of a planar cross section of solids contained in the STEP file. Additionally it supports offsetting of the cross section contour."};
 	options.add_options()
 			("i,in", "Input file", cxxopts::value<std::string>())
 			("o,out", "Output file", cxxopts::value<std::string>())
@@ -195,6 +408,7 @@ int main(int argc, char* argv[]) {
 			("c,content", "List content (solids)")
 			("s,select", "Select solids by name or index (comma seperated list, index starts with 1)", cxxopts::value<std::vector<std::string>>())
 			("d,deflection", "deflection", cxxopts::value<double>())
+			("t,offset", "offset", cxxopts::value<double>()->default_value("0.0"))
 			("p,plane", "Plane (a,b,c,d), in which a*x + b*y + c*z + d = 0", cxxopts::value<std::vector<double>>())
 			("h,help", "Print usage");
 	try {
@@ -213,16 +427,17 @@ int main(int argc, char* argv[]) {
 			if (!result.count("deflection")) throw std::logic_error{std::string{"Missing option 'deflection'"}};
 			if (!result.count("plane")) throw std::logic_error{std::string{"Missing option 'plane'"}};
 			std::vector<double> plane{result["plane"].as<std::vector<double>>()};
-			if (plane.size() != 4) throw std::logic_error{std::string{"Wrong plane format (nx,ny,nz,d)"}};
+			if (plane.size() != 4) throw std::logic_error{std::string{"Wrong plane format (a,b,c,d)"}};
 			const std::array<double, 4> planeVec{plane[0], plane[1], plane[2], plane[3]};
 			const auto format{result["format"].as<std::string>()};
 			if (format != "xyz" && format != "ply") throw std::logic_error{std::string{"Format '"} + format + "' not supported"};
 			const auto deflection{result["deflection"].as<double>()};
+			const auto offset{result["offset"].as<double>()};
 			std::vector<std::string> select;
 			if (result.count("select")) select = result["select"].as<std::vector<std::string>>();
 			std::vector<NamedSolid> namedSolids;
 			read(inFile, namedSolids);
-			write(outFile, namedSolids, select, deflection, planeVec, format);
+			write(outFile, namedSolids, select, deflection, offset, planeVec, format);
 		}
 		else std::cout << options.help() << std::endl;
 		return EXIT_SUCCESS;
